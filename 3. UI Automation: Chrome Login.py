8e0578e208c4a987360a2970240dca044d311fdf
Yuhwa Jang <132421498+YuhwaUniverse@users.noreply.github.com>
2023-05-18 15:02:52 -0700

    Updated

--- 3. UI Automation: Chrome Login.py
+++ 3. UI Automation: Chrome Login.py
@@ -1,41 +1,35 @@
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-
 import time
 
-
 driver = webdriver.Chrome()
-
-# Wait for up to 10 seconds for elements to appear
-driver.implicitly_wait(10)  
 
 url = 'http://google.com'
 driver.get(url)
 
 
+# Chrome Login
+
 elm = driver.find_element(By.XPATH, '//*[@id="gb"]/div/div[2]/a/span')
 elm.click()
 
-time.sleep(1)
-
-email_input = driver.find_element(By.CLASS_NAME, "whsOnd.zHQkBf")
+email_input = driver.find_element(By.CSS_SELECTOR, 'input[type="email"]')
 email_input.send_keys('testautomation13579')
-elm = driver.find_element(By.ID, "identifierNext").click()
 
-time.sleep(1)
+next_button = driver.find_element(By.ID, 'identifierNext')
+next_button.click()
 
-password_input = driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')
-password_input.send_keys('Test97531')
-elm = driver.find_element(By.ID, "passwordNext").click()
-
-
-# wait for 5 seconds
+# Wait for 5 seconds 
 time.sleep(5)
 
-# close the browser window
-driver.quit()  
+password_input = None
+password_input = driver.find_element(By.CSS_SELECTOR, 'input[type="password"]')
+password_input.send_keys('Test97531')
 
+next_button = driver.find_element(By.ID, "passwordNext")
+next_button.click()
 
+# Wait for 3 seconds 
+time.sleep(3)
+
+driver.quit()
